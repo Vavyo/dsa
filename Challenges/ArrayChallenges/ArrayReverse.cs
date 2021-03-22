6b68a01ba8f1dd250c1e867c040afd03d10507ff
Vavyo <vavyo2121@gmail.com>
2021-03-22 14:24:30 -0500

    create and call reverse method

---
+++ Challenges/ArrayChallenges/ArrayReverse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Challenges.ArrayChallenges
+{
+    class ArrayReverse
+    {
+        public static int[] Reverse(int[] arr)
+        {
+            return arr;
+        }
+    }
+}
