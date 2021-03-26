3ae4fea8727fdd0c7272feaa8cba098572406b28
Vavyo <vavyo2121@gmail.com>
2021-03-26 14:28:53 -0500

    Added linked list

---
+++ DataStructures.tests/LinkedLists/LinkedListTests.cs
@@ -0,0 +1,24 @@
+﻿using DataStructures.LinkedLists;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace DataStructures.tests.LinkedLists
+{
+    
+    public class LinkedListTests
+    {
+        [Fact]
+        public void Empty_list_has_null_Head()
+        {
+            // arrange / act
+            LinkedList list = new LinkedList();
+
+            // assert
+            Assert.Null(list.Head);
+        }
+    }
+}
