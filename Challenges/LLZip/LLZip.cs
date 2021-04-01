32bc2fbaf37c15585ed85a2c1c6a4b37332bb0a5
Vavyo <vavyo2121@gmail.com>
2021-04-01 03:16:46 -0500

    Null return tests done

--- Challenges/LLZip/LLZip.cs
+++ Challenges/LLZip/LLZip.cs
@@ -3,13 +3,25 @@ using System.Collections.Generic;
 using System.Text;
 using DataStructures.LinkedLists;
 
-namespace Challenges.LLZip
+namespace Challenges.LinkedListChallenges
 {
     public class LLZip
     {
-        public LinkedList zipLists(LinkedList list1, LinkedList List2)
+        public static LinkedList ZipLists(LinkedList list1, LinkedList list2)
         {
-            throw new NotImplementedException();
+            if(list1.Head == null) // would be something to check with interviewer about
+            {
+                return list2;
+            }
+
+            Node current1 = list1.Head;
+            Node current2 = list2.Head;
+
+            while(current1 != null)
+            {
+                current1 = current1.Next;
+            }
+            return list1;
         }
     }
 }
