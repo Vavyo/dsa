0bf6b4b967a61934dc4a01968b5ac1c7f7b92a11
Vavyo <vavyo2121@gmail.com>
2021-04-01 02:55:19 -0500

    zipLists method and LinkedList reference

--- Challenges/LLZip/LLZip.cs
+++ Challenges/LLZip/LLZip.cs
@@ -1,11 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using DataStructures.LinkedLists;
 
 namespace Challenges.LLZip
 {
     public class LLZip
     {
-
+        public LinkedList zipLists(LinkedList list1, LinkedList List2)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
