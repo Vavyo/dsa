bcce687b088a7232dcf46add2201e59fdafd58d7
Vavyo <vavyo2121@gmail.com>
2021-04-02 17:05:02 -0500

    test scaffolding

---
+++ DataStructures.tests/StackAndQueue/StackTests.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace DataStructures.StackAndQueue
+{
+    public class StackTests
+    {
+        [Fact]
+        public void Top_is_null_on_stack_creation()
+        {
+            // arrange
+            // act
+            // assert
+        }
+        [Fact]
+        public void Push_adds_top_to_value()
+        {
+            // arrange
+            // act
+            // assert
+        }
+        [Fact]
+        public void Push_works_multiple_times()
+        {
+            // arrange
+            // act
+            // assert
+        }
+        [Fact]
+        public void Pop_returns_node_value()
+        {
+            // arrange
+            // act
+            // assert
+        }
+        [Fact]
+        public void Pop_can_empty_stack()
+        {
+            // arrange
+            // act
+            // assert
+        }
+        [Fact]
+        public void Peek_returns_next_value_on_the_stack()
+        {
+            // arrange
+            // act
+            // assert
+        }
+        [Fact]
+        public void Pop_or_Peek_on_empty_stack_throws_exception()
+        {
+            // arrange
+            // act
+            // assert
+        }
+    }
+}
