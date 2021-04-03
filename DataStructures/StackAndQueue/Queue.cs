4e45ff217a1753918f633ceba3ff45434c0e01dd
Vavyo <vavyo2121@gmail.com>
2021-04-02 21:44:16 -0500

    queue scaffolding

--- DataStructures/StackAndQueue/Queue.cs
+++ DataStructures/StackAndQueue/Queue.cs
@@ -6,6 +6,22 @@ namespace DataStructures.StackAndQueue
 {
     public class Queue<T>
     {
-        
+        private Node<T> Front { get; set; }
+        public void Enqueue(T value)
+        {
+            throw new NotImplementedException();
+        }
+        public void Dequeue()
+        {
+            throw new NotImplementedException();
+        }
+        public void Peek()
+        {
+            throw new NotImplementedException();
+        }
+        public bool isEmpty()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
