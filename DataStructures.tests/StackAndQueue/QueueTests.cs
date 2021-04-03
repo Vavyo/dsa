330b5450c963a94277bd435b38454e0e0597a5e9
Vavyo <vavyo2121@gmail.com>
2021-04-02 21:49:08 -0500

    queue test scaffolding

---
+++ DataStructures.tests/StackAndQueue/QueueTests.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace DataStructures.StackAndQueue
+{
+    public class QueueTests
+    {
+        [Fact]
+        public void Enqueue_works()
+        {
+            // arrange
+            // act
+            // assert
+        }
+        [Fact]
+        public void Enqueue_works_multiple_times()
+        {
+            // arrange
+            // act
+            // assert
+        }
+        [Fact]
+        public void Dequeue_works_for_correct_value()
+        {
+            // arrange
+            // act
+            // assert
+        }
+        [Fact]
+        public void Peek_shows_the_next_value()
+        {
+            // arrange
+            // act
+            // assert
+        }
+        [Fact]
+        public void Dequeue_multiple_times_can_empty_the_queue()
+        {
+            // arrange
+            // act
+            // assert
+        }
+        [Fact]
+        public void Can_instantiate_an_empty_queue()
+        {
+            // arrange
+            // act
+            // assert
+        }
+        [Fact]
+        public void Dequeue_or_Peek_on_and_empty_queue_throws_exception()
+        {
+            // arrange
+            // act
+            // assert
+        }
+
+    }
+}
