2e87a58fd9bab92c3d0078f3a5461bdc9b70b246
Vavyo <vavyo2121@gmail.com>
2021-04-05 14:09:13 -0500

    tests 1 and 2

--- Challenges.tests/QueueAndStackChallenges/PseudoQueueTests.cs
+++ Challenges.tests/QueueAndStackChallenges/PseudoQueueTests.cs
@@ -9,12 +9,61 @@ namespace Challenges.tests.QueueAndStackChallenges
     public class PseudoQueueTests
     {
         [Fact]
-        public void Enqueue_works()
+        public void Enqueue_and_IsEmpty_works()
+        {
+            // arrange
+            PseudoQueue<int> queue = new PseudoQueue<int>();
+            // assert
+            Assert.True(queue.IsEmpty);
+            // act
+            queue.Enqueue(1);
+            // assert
+            Assert.False(queue.IsEmpty);
+        }
+        [Fact]
+        public void Dequeue_works()
+        {
+            // arrange
+            PseudoQueue<int> queue = new PseudoQueue<int>();
+            queue.Enqueue(1);
+            // act
+            int result = queue.Dequeue();
+            // assert
+            Assert.True(queue.IsEmpty);
+            Assert.Equal(1, result);
+            // arrange
+            queue.Enqueue(3);
+            queue.Enqueue(5);
+            // act
+            result = queue.Dequeue();
+            // assert
+            Assert.False(queue.IsEmpty);
+            Assert.Equal(3, result);
+        }
+        [Fact]
+        public void Peek_works()
         {
             // arrange
             PseudoQueue<int> queue = new PseudoQueue<int>();
             // act
             // assert
         }
+        [Fact]
+        public void Dequeue_and_Peek_dont_break_IsEmpty()
+        {
+            // arrange
+            PseudoQueue<int> queue = new PseudoQueue<int>();
+            // act
+            // assert
+        }
+        [Fact]
+        public void Enqueue_works_after_Dequeue()
+        {
+            // arrange
+            PseudoQueue<int> queue = new PseudoQueue<int>();
+            // act
+            // assert
+        }
+
     }
 }
