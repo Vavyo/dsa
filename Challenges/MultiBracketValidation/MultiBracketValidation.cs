cc2db9329f8e27e8c8c0e3787769cfb6844dcaee
Vavyo <vavyo2121@gmail.com>
2021-04-07 15:14:11 -0500

    all tests green

--- Challenges/MultiBracketValidation/MultiBracketValidation.cs
+++ Challenges/MultiBracketValidation/MultiBracketValidation.cs
@@ -8,7 +8,46 @@ namespace Challenges.StackAndQueueChallenges
     {
         public static bool CheckBracketBalance(string input)
         {
-            throw new NotImplementedException();
+            Stack<char> stack = new Stack<char>();
+            bool containsBracket = false;
+
+            foreach(char c in input)
+            {
+                    
+                switch (c)
+                {
+                    case '{':
+                        stack.Push(c);
+                        containsBracket = true;
+                        break;
+                    case '[':
+                        stack.Push(c);
+                        containsBracket = true;
+                        break;
+                    case '(':
+                        stack.Push(c);
+                        containsBracket = true;
+                        break;
+                    case '}':
+                        if (!stack.TryPop(out char o ) || o != '{')
+                            return false;
+                        break;
+                    case ']':
+                        // if (stack.Pop() != '[')
+                        if (!stack.TryPop(out o) || o != '[')
+                            return false;
+                        break;
+                    case ')':
+                        if (!stack.TryPop(out o) || o != '(')
+                            return false;
+                        break;
+                    default:
+                        break;
+                }
+            }
+            if (stack.Count != 0 || !containsBracket)
+                return false;
+            return true;
         }
     }
 }
