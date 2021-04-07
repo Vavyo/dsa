ae6759a03cda21f287087b652795a17971319e08
Vavyo <vavyo2121@gmail.com>
2021-04-07 14:38:33 -0500

    initial scafolding

---
+++ Challenges.tests/QueueAndStackChallenges/MultiBracketValidationTests.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Challenges.tests.QueueAndStackChallenges
+{
+    public class MultiBracketValidationTests
+    {
+        [Fact]
+        public void CheckBracketBalance_returns_false_for_an_empty_string()
+        {
+            // arrange
+            // act
+            // assert
+        }
+        [Fact]
+        public void CheckBracketBalance_returns_false_for_a_single_character_string()
+        {
+            // arrange
+            // act
+            // assert
+        }
+        [Fact]
+        public void CheckBracketBalance_returns_false_for_a_string_with_no_brackets()
+        {
+            // arrange
+            // act
+            // assert
+        }
+        [Fact]
+        public void CheckBracketBalance_returns_false_for_a_string_with_unbalanced_brackets()
+        {
+            // arrange
+            // act
+            // assert
+        }
+        [Fact]
+        public void CheckBracketBalance_returns_false_for_an_uneven_number_of_brackets()
+        {
+            // arrange
+            // act
+            // assert
+        }
+        [Fact]
+        public void CheckBracketBalance_returns_false_for_a_string_of_only_opening_brackets()
+        {
+            // arrange
+            // act
+            // assert
+        }
+        [Fact]
+        public void CheckBracketBalance_returns_false_for_a_string_of_only_closing_brackets()
+        {
+            // arrange
+            // act
+            // assert
+        }
+        [Fact]
+        public void CheckBracketBalance_returns_true_for_balanced_brackets()
+        {
+            // arrange
+            // act
+            // assert
+        }
+        [Fact]
+        public void CheckBracketBalance_returns_true_for_balanced_brackets_with_words()
+        {
+            // arrange
+            // act
+            // assert
+        }
+
+    }
+}
---
+++ Challenges/MultiBracketValidation/MultiBracketValidation.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Challenges.StackAndQueueChallenges
+{
+    public class MultiBracketValidation
+    {
+        private static bool CheckBracketBalance(string input)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
