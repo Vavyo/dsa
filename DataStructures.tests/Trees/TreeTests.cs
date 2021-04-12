2d12cf345deb66ab0e02510e6f920302884aa59a
Vavyo <vavyo2121@gmail.com>
2021-04-12 14:27:46 -0500

    preorder

---
+++ DataStructures.tests/Trees/TreeTests.cs
@@ -0,0 +1,25 @@
+﻿using DataStructures.Trees;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace DataStructures.tests.Trees
+{
+    public class TreeTests
+    {
+        [Fact]
+        public void PreOrder_works()
+        {
+            // Arrange
+            Tree<int> tree = new Tree<int>();
+            tree.Root = new Node<int>(5);
+            tree.Left = new Node<int>(7);
+            tree.Right = new Node<int>(12);
+            // Act
+            var result = tree.PreOrder();
+            // Assert
+            Assert.Equal(new[] { 5, 7, 12 }, result);
+        }
+    }
+}
---
+++ DataStructures/Trees/Tree.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructures.Trees
+{
+    public class Tree<T>
+    {
+        public Node<T> Root { get; set; }
+        public Node<T> Left { get => Root.Left; set => Root.Left = value; }
+        public Node<T> Right { get => Root.Right; set => Root.Right = value; }
+
+        public T[] PreOrder()
+        {
+            if (Root == null)
+                throw new InvalidOperationException("Tree is empty");
+            Queue<T> result = new Queue<T>();
+            PreOrder(Root, result);
+            T[] arr = new T[result.Count];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = result.Dequeue();
+            }
+            return arr;
+        }
+        private void PreOrder(Node<T> node, Queue<T> list)
+        {
+            if (node == null)
+                return;
+            list.Enqueue(node.Value);
+            if(node.Left != null)
+            {
+                PreOrder(node.Left, list);
+            }
+            if (node.Right != null)
+            {
+                PreOrder(node.Right, list);
+            }
+            
+        }
+    }
+}
