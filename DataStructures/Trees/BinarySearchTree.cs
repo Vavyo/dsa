ee570c0ac45ed3b24140f16b07903f74e53c365f
Nathan Hall <nhall2121@gmail.com>
2022-05-25 13:33:59 -0500

    Changed Binary search tree to use the new node functionality to add search nodes.

--- DataStructures/Trees/BinarySearchTree.cs
+++ DataStructures/Trees/BinarySearchTree.cs
@@ -4,42 +4,16 @@ using System.Text;
 
 namespace DataStructures.Trees
 {
-    public class BinarySearchTree
+    public class BinarySearchTree<T> : Tree<T> where T : IComparable
     {
-        private Node<int> Root { get; set; }
-        public void Add(int value)
+        public void AddNode(T value)
         {
-            if (AddHelper(Root, value))
-                Root = new Node<int>(value);
-        }
-        private bool AddHelper(Node<int> node, int value)
-        {
-            if (node == null)
-                return true;
-            if (node.Value > value)
-                if (AddHelper(node.Left, value))
-                    node.Left = new Node<int>(value);
-            if (node.Value < value)
-                if (AddHelper(node.Right, value))
-                    node.Right = new Node<int>(value);
-            return false;
-        }
-        public bool Contains(int value)
-        {
-            return ContainsHelper(Root, value);
-        }
-
-        private bool ContainsHelper(Node<int> node, int value)
-        {
-            if (node == null)
-                return false;
-            if (node.Value == value)
-                return true;
-            if (node.Value > value)
-                return ContainsHelper(node.Left, value);
-            if (node.Value < value)
-                return ContainsHelper(node.Right, value);
-            throw new InvalidOperationException("Unexpected error.");
+            Node<T> node = new Node<T>(value);
+            if(Root == null)
+            {
+                Root = node;
+            }
+            Root.AddSearchNode(node);
         }
     }
 }
